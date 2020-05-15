1089bab943f6d66611b6bbb71cb81c571c92fc16
Matteo Bettini <43383555+600mt@users.noreply.github.com>
2020-05-15 21:50:33 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -12,10 +12,10 @@ $(document).mousemove(function(e){
     var pageX = Math.round(e.pageX / w);
     var pageY = Math.round(e.pageY / h);
    // body.css("background-color", "rgb("+pageX+","+pageY+","+pageX+")");
-    rect.css("padding-top", pageY);
-    rect2.css("padding-top", pageY/1.5);
-    rect3.css("padding-top", pageY/2);
-    rect4.css("padding-top", pageY/3);
+    rect.css("padding-top", pageY/4);
+    rect2.css("padding-top", pageY/8);
+    rect3.css("padding-top", pageY/16);
+    rect4.css("padding-top", pageY/25);
 }); 
     
 });
